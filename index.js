80f04cd7e2e2a9e0bb4ea3511bc63b150d3bb6c9
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-16 11:22:43 -0600

    Authentication working

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ server.post('/api/register', (req, res) => {
 
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
-  const hash = bcrypt.hashSync(password, 16)
+  const hash = bcrypt.hashSync(password, 8)
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}! ${hash} ${user.password}` });
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -58,7 +58,7 @@ server.get('/hash', (req, res) => {
 
   //hash the name
 
-  const hash = bcrypt.hashSync(name, 12); // use bcryptjs to hash the name
+  const hash = bcrypt.hashSync(name, 8); // use bcryptjs to hash the name
   res.send(`the hash for ${name} is ${hash}`)
 })
 
